e5ee7b292a2987a4dcfc873ee544d9b625b28238
abelmathew07 <abelmathewyo22@gmail.com>
2024-08-01 11:14:53 +0530

    changed my title page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
